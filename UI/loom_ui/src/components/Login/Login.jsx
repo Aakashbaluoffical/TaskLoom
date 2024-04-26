211c35d9143a64c8ba462ae624178d7f218fc90e
Pooja Das <poojaadas20@gmail.com>
2024-04-26 19:50:51 +0530

    Validation added in email and password input fields on 26thApril024

--- UI/loom_ui/src/components/Login/Login.jsx
+++ UI/loom_ui/src/components/Login/Login.jsx
@@ -1,145 +1,192 @@
-import React from 'react'
+import React, { useState } from "react";
 // import Login_image from '../../assets/login.jpg';
-import './Login.css';
-
-import Avatar from '@mui/material/Avatar';
-
-import Button from '@mui/material/Button';
-
-import CssBaseline from '@mui/material/CssBaseline';
-
-import TextField from '@mui/material/TextField';
-
-import FormControlLabel from '@mui/material/FormControlLabel';
-
-import Checkbox from '@mui/material/Checkbox';
-
-import Link from '@mui/material/Link';
-
-import Grid from '@mui/material/Grid';
-
-import Box from '@mui/material/Box';
-
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import "./Login.css";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+  ThemeProvider,
+} from "@mui/material";
+// import Avatar from "@mui/material/Avatar";
+// import Button from "@mui/material/Button";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import TextField from "@mui/material/TextField";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
+// import Link from "@mui/material/Link";
+// import Grid from "@mui/material/Grid";
+// import Box from "@mui/material/Box";
+// import Typography from "@mui/material/Typography";
+// import Container from "@mui/material/Container";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { createTheme } from "@mui/material/styles";
 
 function Copyright(props) {
-
   return (
-
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-
-      {'Copyright © '}
-
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
-
         Team Loom
-
-      </Link>{' '}
-
+      </Link>{" "}
       {new Date().getFullYear()}
-
-      {'.'}
-
+      {"."}
     </Typography>
-
   );
-
 }
 // TODO remove, this demo shouldn't need to reset the theme.
- 
+
 const defaultTheme = createTheme();
 
 function Login() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-          email: data.get('email'),
-          password: data.get('password'),
-        });
-      };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
+  const isValidEmail = (email) => {
+    // Basic email format validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const isValidPassword = (password) => {
+    // Password strength validation
+    const passwordRegex =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const handleEmailBlur = () => {
+    setEmailError(!isValidEmail(email));
+  };
+
+  const handlePasswordBlur = () => {
+    setPasswordError(!isValidPassword(password));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmailError(!isValidEmail(email));
+    setPasswordError(!isValidPassword(password));
+
+    if (!isValidEmail(email) || !isValidPassword(password)) {
+      return;
+    }
+
+    // If validations pass, you can proceed with the login process
+    // Example: loginUser(email, password);
+  };
 
   return (
     <>
-        <div>
-            <ThemeProvider theme={defaultTheme}>
-<Container component="main" maxWidth="xs">
-{/* <div className='login_image_container'>
+      {/* <div> */}
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          {/* <div className='login_image_container'>
             <img className='login_image'  src={Login_image} alt="No_image"/>
             </div> */}
-<CssBaseline />
-<Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
->
-<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-<LockOutlinedIcon />
-</Avatar>
-<Typography component="h1" variant="h5">
-            Sign in
-</Typography>
-<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-<TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-<TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-<FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-<Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
->
-              Sign In
-</Button>
-<Grid container>
-<Grid item xs>
-<Link href="#" variant="body2">
-                  Forgot password?
-</Link>
-</Grid>
-<Grid item>
-<Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-</Link>
-</Grid>
-</Grid>
-</Box>
-</Box>
-<Copyright sx={{ mt: 8, mb: 4 }} />
-</Container>
-</ThemeProvider>
-        </div>
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                onBlur={handleEmailBlur}
+                error={emailError}
+                helperText={
+                  emailError ? "Please enter a valid email address" : ""
+                }
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onBlur={handlePasswordBlur}
+                error={passwordError}
+                helperText={
+                  passwordError
+                    ? "Password must be at least 8 characters long and include uppercase, lowercase, digit, and special characters"
+                    : ""
+                }
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </ThemeProvider>
+      {/* </div> */}
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
